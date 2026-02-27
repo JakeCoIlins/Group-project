93af9bd8a99c405c7f914981f98d2330456dde95
guest56-booop <jonesdanny697@gmail.com>
2026-02-27 10:05:37 +0000

    update 4

--- Assets/Script/countdown.cs
+++ Assets/Script/countdown.cs
@@ -1,7 +1,10 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 using TMPro;
 using UnityEngine;                                                                                                      
 using UnityEngine.UI;
@@ -12,25 +15,51 @@ public class countdown : MonoBehaviour
     private float TimeLeft;
 
     public TMP_Text TimerText;
+<<<<<<< Updated upstream
         void Start()
         {
         TimeLeft = StartTime;
         TimerText.gameObject.SetActive(true);
         }
     // Update is called once per frame
+=======
+
+    void Start()
+    {
+        TimeLeft = StartTime;
+
+    }
+
+>>>>>>> Stashed changes
     void Update()
     {
         if(TimeLeft > 0) 
         {
             //time tick down
             TimeLeft -= Time.deltaTime;
+<<<<<<< Updated upstream
             TimerText.text = TimeLeft.ToString("0.00");
+=======
+                TimerText.text = TimeLeft.ToString("0.00");
+>>>>>>> Stashed changes
         }
         else
         {
             //Timer Finished
+<<<<<<< Updated upstream
             TimerText.gameObject.SetActive(true);
         }
     }
 }
  //https://www.youtube.com/watch?v=T0atxxGoOlE
+=======
+        }
+
+    }
+    public void starttime() 
+    {
+        TimeLeft = StartTime;
+    }
+}
+ //https://www.youtube.com/watch?v=T0atxxGoOlE
+>>>>>>> Stashed changes
