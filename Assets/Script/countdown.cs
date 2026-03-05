a4ad573f9db5e415ccb675cfa759b03944de536b
guest56-booop <jonesdanny697@gmail.com>
2026-03-05 14:16:29 +0000

    timer update

--- Assets/Script/countdown.cs
+++ Assets/Script/countdown.cs
@@ -1,65 +1,48 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 using TMPro;
 using UnityEngine;                                                                                                      
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class countdown : MonoBehaviour
 {
     public float StartTime;
     private float TimeLeft;
 
     public TMP_Text TimerText;
-<<<<<<< Updated upstream
+
         void Start()
         {
         TimeLeft = StartTime;
         TimerText.gameObject.SetActive(true);
         }
-    // Update is called once per frame
-=======
 
-    void Start()
-    {
-        TimeLeft = StartTime;
-
-    }
-
->>>>>>> Stashed changes
     void Update()
     {
         if(TimeLeft > 0) 
         {
             //time tick down
             TimeLeft -= Time.deltaTime;
-<<<<<<< Updated upstream
+
             TimerText.text = TimeLeft.ToString("0.00");
-=======
+
                 TimerText.text = TimeLeft.ToString("0.00");
->>>>>>> Stashed changes
+
         }
         else
         {
             //Timer Finished
-<<<<<<< Updated upstream
-            TimerText.gameObject.SetActive(true);
-        }
-    }
-}
- //https://www.youtube.com/watch?v=T0atxxGoOlE
-=======
-        }
 
+            TimerText.gameObject.SetActive(true);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
+ //https://www.youtube.com/watch?v=T0atxxGoOlE
     public void starttime() 
     {
         TimeLeft = StartTime;
     }
 }
  //https://www.youtube.com/watch?v=T0atxxGoOlE
->>>>>>> Stashed changes
+
