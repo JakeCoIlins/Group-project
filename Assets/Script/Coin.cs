fdc963c103745741b45f9d5b7fb1c005eecfb72f
JakeCoIlins <92150@student.varndean.ac.uk>
2026-03-17 16:04:55 +0000

    coin

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -4,6 +4,7 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
---
+++ Assets/Script/CoinCollection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+    private int Coin = 0;
+
+    public TextMeshProUGUI CoinText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Coin")
+        {
+            Coin++;
+            CoinText.text = "Coin: " + Coin.ToString();
+            Debug.Log(Coin);
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
